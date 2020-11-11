524c86a83dbe644fdad2a6ede64f6ebd5bc6fb62
Maksim Ruts <tribar.ge@gmail.com>
2020-11-12 01:17:50 +0300

    Adds base allocator class

---
+++ allocator.h
@@ -0,0 +1,42 @@
+//
+// Created by novasurfer on 11/10/20.
+//
+
+#ifndef SC_ALLOCATOR_H
+#define SC_ALLOCATOR_H
+
+#include <cstddef>
+
+namespace sc
+{
+
+    struct allocator
+    {
+	virtual void* allocate(size_t size, size_t align) = 0;
+        virtual void deallocate(void* ptr) = 0;
+        virtual size_t allocated_size(void* ptr) = 0;
+        virtual size_t total_allocated() = 0;
+        virtual ~allocator() {};
+
+        template <typename T, typename... TArgs>
+        T* make_new(TArgs&... args);
+
+        template <typename T, typename... TArgs>
+        T* make_new(const TArgs&... args);
+    };
+
+    template <typename T, typename... TArgs>
+    T* allocator::make_new(TArgs&... args)
+    {
+        return new(allocate(sizeof(T), alignof(T))) T(args...);
+    }
+
+    template <typename T, typename... TArgs>
+    T* allocator::make_new(const TArgs&... args)
+    {
+        return new(allocate(sizeof(T), alignof(T))) T(args...);
+    }
+}
+
+#endif //SC_ALLOCATOR_H
+
