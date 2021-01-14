fdb3380c43d4fdd4b3699455455e499ccb567be8
Maksim Ruts <tribar.ge@gmail.com>
2021-01-14 03:12:37 +0300

    Adds hash functions

---
+++ hash.h
@@ -0,0 +1,24 @@
+#include "MurmurHash3.h"
+#include <bits/stdint-uintn.h>
+#include <cstdint>
+#include <cstring>
+
+namespace sc
+{
+    static constexpr uint32_t HASH_SEED = 0x81D;
+
+    uint32_t hash(uint32_t key)
+    {
+        uint32_t result[1];
+	const uint32_t* ptr = &key;
+        MurmurHash3_x86_32(ptr, sizeof(uint32_t), HASH_SEED, result);
+        return *result;
+    }
+
+    uint32_t hash(const char* key)
+    {
+        uint32_t result[1];
+        MurmurHash3_x86_32(key, strlen(key), HASH_SEED, result);
+        return *result;
+    }
+}
