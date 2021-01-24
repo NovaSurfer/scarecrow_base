678167f47592e8e6f18bb6d5646275fa346e03c6
Maksim Ruts <tribar.ge@gmail.com>
2021-01-24 16:14:04 +0300

    Cleaning up, adds not-found test

--- tests/hashmap_test.cpp
+++ tests/hashmap_test.cpp
@@ -2,11 +2,6 @@
 #include "../heap_alloc.h"
 #include "../linear_alloc.h"
 #include "doctest/doctest.h"
-#include <bits/stdint-uintn.h>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <functional>
 #include <unordered_map>
 
 struct Pod
@@ -20,19 +15,19 @@ inline bool operator==(const Pod& lhs, const Pod& rhs)
     return lhs.a == rhs.a && lhs.b == rhs.b;
 }
 
-char* rand_string(char* str, size_t size)
-{
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJK...";
-    if(size) {
-        --size;
-        for(size_t n = 0; n < size; n++) {
-            int key = rand() % (int)(sizeof charset - 1);
-            str[n] = charset[key];
-        }
-        str[size] = '\0';
-    }
-    return str;
-}
+// char* rand_string(char* str, size_t size)
+// {
+//     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJK...";
+//     if(size) {
+//         --size;
+//         for(size_t n = 0; n < size; n++) {
+//             int key = rand() % (int)(sizeof charset - 1);
+//             str[n] = charset[key];
+//         }
+//         str[size] = '\0';
+//     }
+//     return str;
+// }
 
 TEST_CASE("hash")
 {
@@ -69,15 +64,14 @@ TEST_CASE("hashmap-stress")
     // stl hashmap
     std::unordered_map<uint32_t, Pod> stlmap;
 
-    printf("sizeof myhashmap %lu\n", sizeof(sc::hashmap<uint32_t, Pod>));
-    printf("sizeof stdumap %lu\n", sizeof(std::unordered_map<uint32_t, Pod>));
+    // printf("sizeof myhashmap %lu\n", sizeof(sc::hashmap<uint32_t, Pod>));
+    // printf("sizeof stdumap %lu\n", sizeof(std::unordered_map<uint32_t, Pod>));
 
     srand(0);
 
     // insert
     for(uint32_t i = 0; i < LIMIT; ++i) {
         uint32_t key = static_cast<uint32_t>(rand());
-        printf("key %d\n", key);
         int a = rand();
         int b = rand();
         Pod pod {a, b};
@@ -85,12 +79,12 @@ TEST_CASE("hashmap-stress")
         stlmap[key] = pod;
     }
 
-    for(uint32_t i = 0; i < COUNT; ++i) {
-        printf("KEY: %d, POD: a=%d, b=%d, DIB: %d\n", hmap.kvps[i].key, hmap.kvps[i].value.a,
-               hmap.kvps[i].value.b, hmap.kvps[i].dib);
-    }
+    // for(uint32_t i = 0; i < COUNT; ++i) {
+    //     printf("KEY: %d, POD: a=%d, b=%d, DIB: %d\n", hmap.kvps[i].key, hmap.kvps[i].value.a,
+    //            hmap.kvps[i].value.b, hmap.kvps[i].dib);
+    // }
 
-    printf("------------------------\n");
+    // printf("------------------------\n");
 
     REQUIRE_EQ(LIMIT, hmap.len());
 
@@ -110,15 +104,15 @@ TEST_CASE("hashmap-stress")
         hmap.remove(key);
     }
 
-    for(auto& i : hmap) {
-        printf("AFTER DELETE KEY: %d, POD: a=%d, b=%d, DIB: %d\n", i.key, i.value.a, i.value.b,
-               i.dib);
-    }
+    // for(auto& i : hmap) {
+    //     printf("AFTER DELETE KEY: %d, POD: a=%d, b=%d, DIB: %d\n", i.key, i.value.a, i.value.b,
+    //            i.dib);
+    // }
 
-    for(uint32_t i = 0; i < COUNT; ++i) {
-        printf("KEY: %d, POD: a=%d, b=%d, DIB: %d\n", hmap.kvps[i].key, hmap.kvps[i].value.a,
-               hmap.kvps[i].value.b, hmap.kvps[i].dib);
-    }
+    // for(uint32_t i = 0; i < COUNT; ++i) {
+    //     printf("KEY: %d, POD: a=%d, b=%d, DIB: %d\n", hmap.kvps[i].key, hmap.kvps[i].value.a,
+    //            hmap.kvps[i].value.b, hmap.kvps[i].dib);
+    // }
 }
 
 TEST_CASE("hashmap-add-remove-add")
@@ -128,33 +122,81 @@ TEST_CASE("hashmap-add-remove-add")
     char buff[COUNT];
 
     // my hashmap
-    sc::hashmap<const char*, Pod> hmap(lalloc3);
+    sc::hashmap<uint32_t, Pod> hmap(lalloc3);
     hmap.init(COUNT);
 
     // stl hashmap
-    std::unordered_map<const char*, Pod> stlmap;
+    std::unordered_map<uint32_t, Pod> stlmap;
 
     srand(0);
 
     // insert
     for(uint32_t i = 0; i < COUNT; ++i) {
-        const char* key = rand_string(buff, COUNT);
+        uint32_t key = static_cast<uint32_t>(rand());
         int a = rand();
         int b = rand();
-        printf("rand-a: %d\n", a);
         Pod pod {a, b};
         CHECK(hmap.put(key, pod));
         stlmap[key] = pod;
     }
 
-    for(auto& i : stlmap) {
-        printf("STL KEY: %s, POD: a=%d, b=%d\n", i.first, i.second.a, i.second.b);
-    }
-
-    for(uint32_t i = 0; i < COUNT; ++i) {
-        printf("KEY: %s, POD: a=%d, b=%d, DIB: %d\n", hmap.kvps[i].key, hmap.kvps[i].value.a,
-               hmap.kvps[i].value.b, hmap.kvps[i].dib);
-    }
-
     REQUIRE_EQ(stlmap.size(), hmap.len());
+
+    // remove all
+    for(auto& [key, value] : stlmap) {
+
+        Pod val = hmap.get(key);
+        REQUIRE(val.a != -1);
+        REQUIRE_EQ(value, val);
+        hmap.remove(key);
+    }
+
+    stlmap.clear();
+
+    REQUIRE_EQ(hmap.len(), stlmap.size());
+
+    for(uint32_t i = 0; i < COUNT / 2; ++i) {
+        uint32_t key = static_cast<uint32_t>(rand());
+        int a = rand();
+        int b = rand();
+        Pod pod {a, b};
+        CHECK(hmap.put(key, pod));
+        stlmap[key] = pod;
+    }
+
+    REQUIRE_EQ(hmap.len(), 5);
+
+    // compare
+    for(auto& [key, value] : stlmap) {
+        Pod val = hmap.get(key);
+        REQUIRE(val.a != -1);
+        REQUIRE_EQ(value, val);
+    }
+
+    // for(auto& i : stlmap) {
+    //     printf("STL KEY: %d, POD: a=%d, b=%d\n", i.first, i.second.a, i.second.b);
+    // }
+
+    // for(uint32_t i = 0; i < COUNT; ++i) {
+    //     printf("KEY: %d, POD: a=%d, b=%d, DIB: %d\n", hmap.kvps[i].key, hmap.kvps[i].value.a,
+    //            hmap.kvps[i].value.b, hmap.kvps[i].dib);
+    // }
+
+    // for(auto& i : hmap) {
+    //     printf("AFTER DELETE KEY: %d, POD: a=%d, b=%d, DIB: %d\n", i.key, i.value.a, i.value.b,
+    //            i.dib);
+    // }
+}
+
+TEST_CASE("hashmap-add-get-not-found")
+{
+    sc::hashmap<uint32_t, Pod> hmap(lalloc3);
+    hmap.init(1);
+    hmap.put(1, {1, 1});
+
+    const Pod pod {1,1};
+    CHECK(pod == hmap.get(1));
+
+    const Pod dpod {};
+    CHECK(dpod == hmap.get(2));
 }
