a48d0ed2dfab679de1bec7710e534a8b284a3057
Maksim Ruts <tribar.ge@gmail.com>
2022-01-13 23:57:59 +0300

    Fixes includes

---
+++ tests/shared_test.cpp
@@ -0,0 +1,36 @@
+//
+// Created by novasurfer on 9/3/21.
+//
+
+#ifndef SC_BASE_TEST_SHARED_TEST_CPP_H
+#define SC_BASE_TEST_SHARED_TEST_CPP_H
+
+
+#include "shared.h"
+#include "doctest/doctest.h"
+
+TEST_CASE("shared-ptr-operations")
+{
+    SUBCASE("default constructor")
+    {
+        MESSAGE(sizeof(sc::shared<int*>));
+    }
+
+    SUBCASE("make-and-reset-shared")
+    {
+        auto shared_1 = sc::make_shared<int>(47);
+        auto shared_2 = shared_1;
+
+        {
+            const auto& shared_3 = shared_1;
+
+            MESSAGE(shared_1.use_count());
+            MESSAGE(shared_3.use_count());
+
+        }
+
+        shared_2.reset();
+    }
+}
+
+#endif //SC_BASE_TEST_SHARED_TEST_CPP_H
--- tests/unique_test.cpp
+++ tests/unique_test.cpp
@@ -2,7 +2,7 @@
 // Created by novasurfer on 7/4/21.
 //
 
-#include "../unique.h"
+#include "unique.h"
 #include "doctest/doctest.h"
 
 TEST_CASE("unique-ptr-operations")
@@ -10,6 +10,11 @@ TEST_CASE("unique-ptr-operations")
     SUBCASE("default constructor")
     {
         MESSAGE(sizeof(sc::unique<int*>));
+    }
+
+    SUBCASE("make-unique")
+    {
+        sc::unique<int> un = sc::make_unique<int>(47);
     }
 
 }
