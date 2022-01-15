f044e6355d9985cff28f6a89ab348b906c843f21
Maksim Ruts <tribar.ge@gmail.com>
2022-01-15 22:29:40 +0300

    Updates to constexpr const T* get()

--- arr.h
+++ arr.h
@@ -19,7 +19,7 @@ namespace sc
         constexpr T& operator[](size_t index);
         constexpr const T& operator[](size_t index) const;
         constexpr void insert_at(const T* item, size_t index, size_t size);
-        constexpr T* const get() const;
+        constexpr const T* get() const;
 
     private:
         T data[SIZE];
@@ -45,7 +45,7 @@ namespace sc
     }
 
     template <typename T, size_t SIZE>
-    constexpr T* const arr<T, SIZE>::get() const
+    constexpr const T* arr<T, SIZE>::get() const
     {
         return data;
     }
