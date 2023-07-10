40100dbd116b6ab4ff268945eb217a57f6a22fbd
Max Ruts <tribar.ge@gmail.com>
2023-07-10 18:05:33 +0200

    Fetching doctest directrly from github

---
+++ tests/doctest.cmake
@@ -0,0 +1,12 @@
+
+include(FetchContent)
+FetchContent_Declare(doctest
+        GIT_REPOSITORY https://github.com/doctest/doctest.git)
+FetchContent_MakeAvailable(doctest)
+
+if(NOT doctest_SOURCE_DIR)
+    message(FATAL "No doctest was found, check FetchContent declaration")
+else()
+    message("Doctest sources location: ${doctest_SOURCE_DIR}")
+endif()
+
