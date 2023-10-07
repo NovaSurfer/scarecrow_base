5f01513ee0754e144d1c7643bc005b1d6adf0245
Maksim Ruts <tribar.ge@gmail.com>
2023-10-07 04:46:40 +0200

    Adds uchar, uintptr

--- sc_types.h
+++ sc_types.h
@@ -19,5 +19,7 @@ using u64 = uint64_t;
 using f32 = float;
 using f64 = double;
 using size_t = std::size_t;
+using uchar = unsigned char;
+using uintptr = uintptr_t;
 
 #endif //SC_MEMORY_SC_TYPES_H
