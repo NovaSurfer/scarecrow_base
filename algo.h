742e5b1e2f2e388c3c288d0b40a28bd0cbb3c611
Maksim Ruts <tribar.ge@gmail.com>
2026-02-15 04:31:06 +0100

    adds max & max_value functions

--- algo.h
+++ algo.h
@@ -7,4 +7,31 @@ namespace sc
     {
         return a > b ? a : b;
     }
+
+    template <typename T>
+    constexpr const T& max(T& value)
+    {
+        return value;
+    }
+
+    template <typename T, typename... Ts>
+    constexpr const T& max(T& first, Ts&... rest)
+    {
+        const T& tail_max = max(rest...);
+        return first > tail_max ? first : tail_max;
+    }
+
+    template <typename T>
+    constexpr T max_value(T v)
+    {
+        return v;
+    }
+
+    template <typename T, typename... Ts>
+    constexpr T max_value(T v, Ts... vs)
+    {
+        const T rest_max = max_value(vs...);
+        return (v > rest_max) ? v : rest_max;
+    }
+
 } // namespace sc
